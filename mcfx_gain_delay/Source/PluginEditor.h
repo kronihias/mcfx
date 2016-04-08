29a6dfdd3202759e607c70b3815da284fdb26f26
Matthias Kronlachner <m.kronlachner@gmail.com>
2016-04-08 20:22:14 +0200

    add header

--- mcfx_gain_delay/Source/PluginEditor.h
+++ mcfx_gain_delay/Source/PluginEditor.h
@@ -1,21 +1,21 @@
 /*
-  ==============================================================================
-
-  This is an automatically generated GUI class created by the Introjucer!
-
-  Be careful when adding custom code to these files, as only the code within
-  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
-  and re-saved.
-
-  Created with Introjucer version: 3.1.1
-
-  ------------------------------------------------------------------------------
-
-  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright 2004-13 by Raw Material Software Ltd.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file is part of the mcfx (Multichannel Effects) plug-in suite.
+ Copyright (c) 2013/2014 - Matthias Kronlachner
+ www.matthiaskronlachner.com
+ 
+ Permission is granted to use this software under the terms of:
+ the GPL v2 (or any later version)
+ 
+ Details of these licenses can be found at: www.gnu.org/licenses
+ 
+ ambix is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+ ==============================================================================
+ */
 
 #ifndef __JUCE_HEADER_2351554F350E59C5__
 #define __JUCE_HEADER_2351554F350E59C5__
